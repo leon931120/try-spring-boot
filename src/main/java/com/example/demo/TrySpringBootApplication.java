2cabc3b3cac29b395877bf5cf5b7807c8ce9e543
刘小蔚 <1023615397@qq.com>
2019-08-23 07:57:57 +0800

    >更新

--- src/main/java/com/example/demo/TrySpringBootApplication.java
+++ src/main/java/com/example/demo/TrySpringBootApplication.java
@@ -4,10 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.Environment;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 /**
  * @projectName try-spring-boot
@@ -20,10 +17,13 @@ import java.net.UnknownHostException;
  * <p>Copyright: Copyright (C) 2012-2019</p>
  * <p>Company: Beijing GS Technology Co., Ltd</p>
  */
+// spring boot 的启动注解 ，内部实现了自动装配 ，内置tomcat
 @SpringBootApplication
+// 日志注解
 @Slf4j
 public class TrySpringBootApplication {
 
+    // spring boot 的 启动类
     public static void main(String[] args) {
         ConfigurableApplicationContext application = SpringApplication.run(TrySpringBootApplication.class, args);
         log.info("=======================================");
--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -16,13 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
  * <p>Copyright: Copyright (C) 2012-2019</p>
  * <p>Company: Beijing GS Technology Co., Ltd</p>
  */
+// spring 常用注解之一  标记此类为controller层，把这个类交个spring容器管理，实现ioc与di
 @RestController
 @Slf4j
+/**  spring 常用注解之一 可以实现在类上也可以实现在方法上
+ *  为方法提供路径url
+ *  服务器地址+端口+类url+方法url 为访问此方法的全路径
+ *  example: localhost:8080/com/test
+ */
 @RequestMapping("/com")
 public class TestController {
 
+    // 使用get 方法
     @GetMapping("/test")
     public String test(){
+        // return 字符串
         return "hello world";
     }
 }

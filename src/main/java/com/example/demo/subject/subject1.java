fee7536fecc0cbb44a871f89c7912aba813f9f84
刘小蔚 <1023615397@qq.com>
2019-08-27 10:01:50 +0800

    >subject1

---
+++ src/main/java/com/example/demo/subject/subject1.java
@@ -0,0 +1,24 @@
+package com.example.demo.subject;
+
+/**
+ * @projectName try-spring-boot
+ * @packageName com.example.demo.subject
+ * @className subject1
+ * @author： liuwei
+ * @version： 1.0
+ * @since： 2019-08-27 09:59
+ * <p>Description: ${DESCRIPTION} </p>
+ * <p>Copyright: Copyright (C) 2012-2019</p>
+ * <p>Company: Beijing GS Technology Co., Ltd</p>
+ */
+public class subject1 {
+
+    public static void main(String[] args) {
+        int i  =  0;
+        int i1= i++;
+        int i2= ++i1;
+        /*about i1 and i2 answer?*/
+        System.out.println(i1);
+        System.out.println(i2);
+    }
+}
